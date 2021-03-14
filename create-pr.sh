4c5eaa0f47ad222c5aba54d96158e2156b2ebebc
kenzo <kenzou.kenzou104809@gmail.com>
2021-03-14 10:21:20 +0900

    fix create-pr.sh

--- create-pr.sh
+++ create-pr.sh
@@ -1,5 +1,10 @@
 # !/bin/bash
 
+if [ $# != 1 ]; then
+  echo "PRのタイトルを入力して下さい。"
+  exit 1
+fi
+
 # 現在のブランチ名を取り出す
 # @see https://stackoverflow.com/questions/2111042/how-to-get-the-name-of-the-current-git-branch-into-a-variable-in-a-shell-script
 branch=$(git symbolic-ref --short HEAD)
@@ -11,5 +16,5 @@ fi
 
 # GitHub CLIでPRを作成する
 # TODO: IssueのタイトルをPRのタイトルにする
-gh pr create --title "Close $issue_num" --body "close #$issue_num"
+gh pr create --title "$1" --body "close #$issue_num"
 echo "PR created!"
