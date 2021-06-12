61d5bad396f0b9f66762208cedca5d29ce52f7e7
kenzo <kenzou.kenzou104809@gmail.com>
2021-06-12 12:54:59 +0900

    feat: ファイルをconvert

--- utils/convert-img.sh
+++ utils/convert-img.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 # TODO: 引数をチェック
-# 引数: quality(数値/必須), ファイル名(文字列/必須), 変換後拡張子(文字列/任意)
+# 引数: ファイル名(文字列/必須), 変換後拡張子(文字列/任意)
 
-# TODO: 画像をImageMagickで変換処理
-# TODO: jpegにする
+filename=${1%.*}
+
+# TODO: qualityは30-80まで10刻みで実行
+convert -quality 30% $1 $filename-30.jpeg
+convert -quality 40% $1 $filename-40.jpeg
+
+# for file in *.jpeg
+# do
+#   convert -quality 50% $file ./00_compress/${file%.*}.jpeg
+#   rm $file
+# done
