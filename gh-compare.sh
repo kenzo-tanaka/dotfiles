151bc6273bd7ba9bc12eb0a9a1ebcd9351ac64e6
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-12-20 20:12:52 +0900

    add gh-compare script

---
+++ gh-compare.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+REMOTE=`git config --get remote.origin.url`
+FROM_COMMIT=`git rev-parse HEAD~3`
+TO_COMMIT=`git rev-parse HEAD`
+
+if [[ ${REMOTE} =~ ^https ]]; then
+    REMOTE_URL=`echo ${REMOTE} | sed -e "s/.git//g"`
+else
+    REMOTE_URL="https://"`echo ${REMOTE} | sed -e "s/.git//g" | sed -e "s/:/\//g"`
+fi
+
+echo "${REMOTE_URL}/compare/${FROM_COMMIT}...${TO_COMMIT}"
