c427910bdad311dfa90c2f985e40bd276d412d0c
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-01-25 20:26:19 +0900

    呼び出し側から引数でcomitter nameを渡せるようにした

--- utils/gh-report.sh
+++ utils/gh-report.sh
@@ -2,6 +2,13 @@
 
 # TODO: gh command check
 
+if [ $# != 1 ]; then
+  echo 'コミッター名を指定してください。'
+  exit 1
+fi
+
+comitter=$1
+
 # e.g. - [#1 title](https://github.com/test/pull/1)
 md_list_format='"- [#" + (.number|tostring) + " " +  .title + "](" + .url + ")"'
-gh pr list -a kenzo-tanaka --state closed --json url,title,number -q ".[] | .result = $md_list_format | .result"
+gh pr list -a $1 --state closed --json url,title,number -q ".[] | .result = $md_list_format | .result"
