02a20da78a99d4953285832ce66f1fdfd38fc0a4
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-01-25 20:58:53 +0900

    引数で配列を受け取る
    close #29

--- utils/gh-report.sh
+++ utils/gh-report.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-if [ $# != 1 ]; then
+if [ $# == 0 ]; then
   echo 'コミッター名を指定してください。'
   exit 1
 fi
 
-comitter=$1
+function closed_pull_requests() {
+  md_list_format='"- [#" + (.number|tostring) + " " +  .title + "](" + .url + ")"'
+  monday=$(date -vMonw "+%Y-%m-%d")
+  friday=$(date -vFriw "+%Y-%m-%d")
+  
+  gh pr list -a $1 --search "closed:$monday..$friday" --state closed --json url,title,number -q ".[] | .result = $md_list_format | .result"
+}
 
-# e.g. - [#1 title](https://github.com/test/pull/1)
-md_list_format='"- [#" + (.number|tostring) + " " +  .title + "](" + .url + ")"'
-monday=$(date -vMonw "+%Y-%m-%d")
-friday=$(date -vFriw "+%Y-%m-%d")
-gh pr list -a $1 --search "closed:$monday..$friday" --state closed --json url,title,number -q ".[] | .result = $md_list_format | .result"
+committers=("$@")
+committers_length=$#
+
+for i in $(seq $committers_length)
+do
+  closed_pull_requests ${committers[$i-1]}
+done
