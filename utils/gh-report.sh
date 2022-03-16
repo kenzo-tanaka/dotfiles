ce7247ff41943d1cb204b4b703afb5ff54ddae9c
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-03-16 21:35:37 +0900

    doing | done

--- utils/gh-report.sh
+++ utils/gh-report.sh
@@ -5,14 +5,21 @@ if [ $# == 0 ]; then
   exit 1
 fi
 
-function pull_requests() {
-  md_list_format='"- #" + (.number|tostring) + " " +  .title + " [" + .state + "]"'
-  monday=$(date -vMonw "+%Y-%m-%d")
-  friday=$(date -vFriw "+%Y-%m-%d")
+monday=$(date -vMonw "+%Y-%m-%d")
+friday=$(date -vFriw "+%Y-%m-%d")
+
+function closed_pull_requests() {
+  md_list_format='"- #" + (.number|tostring) + " " +  .title + " [done]"'
   committer_name=$1
 
-  echo -e "\n""$committer_name""\n"
   gh pr list -A $committer_name --search "closed:$monday..$friday" --state closed --json url,title,number,state -q ".[] | .result = $md_list_format | .result"
+}
+
+function open_pull_requests() {
+  md_list_format='"- #" + (.number|tostring) + " " +  .title + " [doing]"'
+  committer_name=$1
+
+  gh pr list -A $committer_name --state open --json url,title,number,state -q ".[] | .result = $md_list_format | .result"
 }
 
 committers=("$@")
@@ -20,5 +27,7 @@ committers_length=$#
 
 for i in $(seq $committers_length)
 do
-  pull_requests ${committers[$i-1]}
+  echo -e "\n""${committers[$-1]}""\n"
+  closed_pull_requests ${committers[$i-1]}
+  open_pull_requests ${committers[$i-1]}
 done
