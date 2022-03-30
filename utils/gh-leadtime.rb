b9e1bb5a73951d09e8736165dafd5ff8c39627b8
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-03-30 15:31:33 +0900

    Add option parser

--- utils/gh-leadtime.rb
+++ utils/gh-leadtime.rb
@@ -1,39 +1,48 @@
 require 'json'
 require 'time'
 
-input = `gh pr list -A kenzo-tanaka --search "merged:2022-03-15" --state merged --json url,title,createdAt,mergedAt`
-results = JSON.parse(input)
+require 'optparse'
 
-class Person
-  def initialize(name:, pull_requests:)
-    @name = name
-    @pull_requests = pull_requests
-  end
+opt = OptionParser.new
+options = {}
+opt.on('-u', '--user ITEM', 'user list') { |v| options[:users] = v }
+opt.parse(ARGV)
 
-  def summary_text
-    result = "#{@name}\n"
-    leadtime = 0
+p options
 
-    @pull_requests.each do |pull_request|
-      text = <<~TEXT
-        - #{pull_request['title']} (lead time: #{lead_time(pull_request)} hour)
-      TEXT
-      result += text
-      leadtime += lead_time(pull_request)
-    end
-    leadtime /= @pull_requests.length
-    result += "average lead time: #{leadtime.floor(2)} hour"
+# input = `gh pr list -A kenzo-tanaka --search "merged:2022-03-15" --state merged --json url,title,createdAt,mergedAt`
+# results = JSON.parse(input)
 
-    result
-  end
+# class Person
+#   def initialize(name:, pull_requests:)
+#     @name = name
+#     @pull_requests = pull_requests
+#   end
 
-  def lead_time(pull_request)
-    merged_at = Time.parse(pull_request['mergedAt'])
-    created_at = Time.parse(pull_request['createdAt'])
+#   def summary_text
+#     result = "#{@name}\n"
+#     leadtime = 0
 
-    ((merged_at - created_at) / 3600).floor 2
-  end
-end
+#     @pull_requests.each do |pull_request|
+#       text = <<~TEXT
+#         - #{pull_request['title']} (lead time: #{lead_time(pull_request)} hour)
+#       TEXT
+#       result += text
+#       leadtime += lead_time(pull_request)
+#     end
+#     leadtime /= @pull_requests.length
+#     result += "average lead time: #{leadtime.floor(2)} hour"
 
-p = Person.new(name: 'kenzo-tanaka', pull_requests: results)
-puts p.summary_text
+#     result
+#   end
+
+#   def lead_time(pull_request)
+#     merged_at = Time.parse(pull_request['mergedAt'])
+#     created_at = Time.parse(pull_request['createdAt'])
+
+#     ((merged_at - created_at) / 3600).floor 2
+#   end
+# end
+
+# p = Person.new(name: 'kenzo-tanaka', pull_requests: results)
+# puts p.summary_text
