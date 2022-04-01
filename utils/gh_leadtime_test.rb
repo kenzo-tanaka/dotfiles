b04ff72056276bbb468a338e200d9ad383a034ae
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-04-01 21:00:09 +0900

    chore: classを移動

--- utils/gh_leadtime_test.rb
+++ utils/gh_leadtime_test.rb
@@ -1,5 +1,37 @@
 require 'minitest/autorun'
 
+class Performance
+  def initialize(name:, pull_requests:)
+    @name = name
+    @pull_requests = pull_requests
+  end
+
+  def summary_text
+    result = "#{@name}\n"
+    return result if @pull_requests.length == 0
+
+    leadtime = 0
+    @pull_requests.each do |pull_request|
+      text = <<~TEXT
+        - #{pull_request['title']} (lead time: #{lead_time(pull_request)} hour)
+      TEXT
+      result += text
+      leadtime += lead_time(pull_request)
+    end
+    leadtime /= @pull_requests.length
+    result += "average lead time: #{leadtime.floor(2)} hour"
+
+    result
+  end
+
+  def lead_time(pull_request)
+    merged_at = Time.parse(pull_request['mergedAt'])
+    created_at = Time.parse(pull_request['createdAt'])
+
+    ((merged_at - created_at) / 3600).floor 2
+  end
+end
+
 class SampleTest < Minitest::Test
   def test_1
     expected = true
