b7b7ecfd9da4e37c2039740db3a5f5c4cd6a9725
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-05-08 18:09:26 +0900

    Add gh issue to md script

---
+++ utils/gh_issue_to_md.rb
@@ -0,0 +1,69 @@
+require 'bundler/inline'
+
+gemfile do
+  source 'https://rubygems.org'
+  gem 'holiday_japan'
+  gem 'graphql-client'
+end
+
+require 'graphql/client'
+require 'graphql/client/http'
+
+module GitHubAPI
+  HTTP = GraphQL::Client::HTTP.new('https://api.github.com/graphql') do
+    def headers(context)
+      {
+        "Authorization" => "Bearer #{ENV['ACCESS_TOKEN']}"
+      }
+    end
+  end
+  Schema = GraphQL::Client.load_schema(HTTP)
+  Client = GraphQL::Client.new(schema: Schema, execute: HTTP)
+end
+
+class GhIssue
+  QUERY = GitHubAPI::Client.parse <<-GraphQL
+    query {
+      repository(owner: "kenzo-tanaka", name: "nextJsBlog") {
+				issues(
+					first: 3
+					orderBy: { field: CREATED_AT, direction: DESC }
+					labels: "article-publish"
+					states: OPEN
+				) {
+					nodes {
+						title
+						body
+						updatedAt
+						number
+					}
+				}
+			}
+    }
+  GraphQL
+
+  def self.call
+    res = exec_query
+    res.data.repository.issues.nodes
+  end
+
+  def self.exec_query
+    GitHubAPI::Client.query(QUERY)
+  end
+end
+
+nodes = GhIssue.call
+nodes.each do |node|
+  md_body = <<~TEXT
+		---
+		title: '#{node.title}'
+		date: '#{node.updated_at}'
+		category: 'dev'
+		---
+
+		#{node.body}
+  TEXT
+
+  File.open("gh-issue-#{node.number}.md", "w") { |f| f.print md_body }
+end
+
